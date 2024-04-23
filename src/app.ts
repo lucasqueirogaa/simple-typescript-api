5d124bf2dd30a72f6c3fecde4ebe5578eff04b1f
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-04-23 17:58:32 -0300

    feat: Initial config on app file with .env and gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Deu boa!!")
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+app.listen(process.env.PORT, async () => {
+  console.log(`Aplicação esta funcionando na porta ${process.env.PORT}`);
+});
