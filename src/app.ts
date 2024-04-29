29cfffa91f181b548f8e24e640c2b8c4cd208b9c
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-04-29 19:40:17 -0300

    feat: Creating morgan

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import express from "express";
 import db from "../config/db";
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morgan";
 
 const app = express();
 app.use(express.json());
+app.use(morganMiddleware);
 dotenv.config();
 
 app.listen(process.env.PORT, async () => {
