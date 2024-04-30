877b0152bb1dcbab86a145c97c68b5a7de2d4652
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-04-29 21:32:20 -0300

    feat: Creat validation for movie

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,14 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { createMovie } from "./controllers/movieController";
+
+import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/movieValidation";
 
 const router = Router();
 
-export default router.post("/movie", createMovie);
+export default router.post(
+  "/movie",
+  movieCreateValidation(),
+  validate,
+  createMovie
+);
