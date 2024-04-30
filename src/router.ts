9bf29367bf5e163874fd58461ef5c93b67571c63
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-04-29 21:35:52 -0300

    feat: Create route to get movie by id

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
-import { createMovie } from "./controllers/movieController";
+import { createMovie, findMovieById } from "./controllers/movieController";
 
 import { validate } from "./middleware/handleValidation";
 import { movieCreateValidation } from "./middleware/movieValidation";
 
 const router = Router();
 
-export default router.post(
-  "/movie",
-  movieCreateValidation(),
-  validate,
-  createMovie
-);
+export default router
+  .post("/movie", movieCreateValidation(), validate, createMovie)
+  .get("/movie/:id", findMovieById);
